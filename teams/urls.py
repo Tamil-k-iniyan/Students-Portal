4f8d0db9f0718c3ba71ac945832e803b4a6a5e9b
Samdurai Ruban <samdurairuban@karunya.edu.in>
2026-03-17 23:58:55 +0530

    My upload feature and fixes

--- teams/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_file
-from django.contrib.auth.views import LogoutView
-
-urlpatterns = [
-    path('upload/', upload_file, name='upload_file'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-]
